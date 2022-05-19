8a1146f0546c09e531c92da78aada9feed5bc492
david-soto-m <davidsotomarchena@protonmail.com>
2022-05-19 22:39:11 +0200

    modelado

--- create_model.m
+++ create_model.m
@@ -4,5 +4,5 @@ output = out.T_room1.Data - 20.65;
 input = input( 10/Tint: length(input),:);
 output = output( 10/Tint: length(output),:);
 systemIdentification;
-pause;
-save data/models tf1 ss1;
+% pause;
+% save data/models tf1 ss1;
