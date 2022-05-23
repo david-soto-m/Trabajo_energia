4a922844c8fac26678d98b5d40f0fab1ec54c131
martajlopez <martajlopez98@gmail.com>
2022-05-23 09:34:03 +0200

    Added MPC

--- init_fcn.m
+++ init_fcn.m
@@ -2,10 +2,12 @@
 mex ff3dm.c lib/ffm_3d_modulator.c;
 
 %% Time related
+Tsim = 20;
 Tint = 5e-5; % s
 Ts = 0.0005; % s
 % 2 khz ↑
-T_pred = Ts; % Tendría que escoger esto, yo vería 0.02 o como mucho 0.001
+T_pred = Ts*10; % Tendría que escoger esto, yo vería 0.02 o como mucho 0.001
+T_Enable = 0.36;
 
 %% Exterior
 T_ext = 15; % deg
@@ -36,3 +38,45 @@ K_cond = Window_area * k_glass / esp; % W/K
 tf_cont=tf(1/C_V,[1 K_cond/C_V]); %% En función de transferencia contínua
 ss2c= ss(tf_cont); % En ecuaciónes de estado continua
 ss2 = c2d(ss2c, T_pred, 'tustin'); % En ecuaciónes de estado discretas
+
+%% MPC Control
+% Tm = Ts*20; % Tiempo de muestreo del control
+nx = size(ss2.A,2); % Numero de estados
+nu = size(ss2.B,2); % Numero de acciones de control
+lambda = 0.1;
+U0 = 0;
+X0 = 0;
+Kobs = 1;
+N2 = 10; % Horizonte de prQediccion
+Nu = 5; % Horizonte de control
+
+model = ss2;
+
+Temp_ref = [17*ones(Tsim/(T_pred*2),1); 22*ones(Tsim/(T_pred*2),1)];
+
+M = [model.A model.B; zeros(nu,nx) eye(nu)];
+N = [model.B; eye(nu)];
+Q = [model.C zeros(1,nu)]; % zeros(ny,nu)
+
+% y_pred = F* x_pred + H *U
+% Calculamos F = [Q*M; Q*M^2; ....; Q*M^(N2)
+F = zeros(N2,(nx+nu)); %N2 filas, Nx+Nu columnas(dimension de M)
+for i=1:N2
+    F(i,:) = Q*(M^(i));
+end
+
+% Calculamos H ; Matriz triangular con Hij = Q*M^(i-j)*N, dimensiones N2 x Nu
+H = zeros(N2,Nu);
+for i = 1:N2
+    for j = 1:Nu
+        if i>=j
+            H(i,j) = Q*M^(i-j)*N;
+        else
+            H(i,j) = 0;
+        end
+    end
+end
+
+% Accion de control y salida
+K1 = inv(H'*H + lambda*eye(Nu))*H';
+K = K1(1,:); % Solo interesa la primera fila
